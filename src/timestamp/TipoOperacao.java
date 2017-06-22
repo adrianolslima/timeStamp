30f55f241513b0507aa7660c2e46a0880610f89e
Adriano <Adriano@PC-Adriano>
2017-06-21 23:27:16 -0300

    Incluir enum TipoOperacao

---
+++ src/timestamp/TipoOperacao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timestamp;
+
+/**
+ *
+ * @author Adriano
+ */
+public enum TipoOperacao {
+    S,  //Start
+    R,  //Read
+    W,  //Write
+    C;  //Commit    
+}
