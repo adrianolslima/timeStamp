e7b20c702c1e58d8cb75232955f983be006c44be
Adriano <Adriano@PC-Adriano>
2017-06-22 01:03:07 -0300

    Retirar os atributos "dado" e "valor" como parametros do construtor

---
+++ src/timestamp/Operador.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timestamp;
+
+/**
+ *
+ * @author Adriano
+ */
+public class Operador {
+    
+    private Tabela tabela;
+    private Escalonador escalonador;
+    
+    private Transacao transacaoAtiva;
+
+    public Operador(Escalonador escalonador) {
+        this.tabela = new Tabela();
+        this.escalonador = escalonador;
+    }
+
+    public void atualizar() {
+        this.transacaoAtiva = this.escalonador.getTransacaoAtiva();
+    }
+    
+    public void executar() {
+        Operacao operacao;
+        operacao = this.transacaoAtiva.getOperacao();
+        char dado = operacao.getDado();
+//        int valor = operacao.getValor();
+        long ts = transacaoAtiva.getTimeStamp();        
+        
+        switch (operacao.getTipo()) {
+            case S :
+                break;
+            case R :
+                tabela.read(dado, ts);
+                break;
+            case W :
+                tabela.write(dado, valor, ts);
+                break;
+            case C :
+                break;
+            
+        }
+    }
+    
+}
