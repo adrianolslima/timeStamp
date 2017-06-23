95ed1a76fd43821c2336eb3d6f724ecbe3d86248
Adriano <Adriano@PC-Adriano>
2017-06-23 00:03:26 -0300

    Implementar o método "gerarTransacao" na classe Gerador.

--- src/timestamp/Gerador.java
+++ src/timestamp/Gerador.java
@@ -7,22 +7,59 @@ package timestamp;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Random;
 
 /**
  *
  * @author Adriano
  */
 public class Gerador {
-    
+
+    private long ponteiro;  //Define o id da transacao
+
     public Gerador() {
-        
+        this.ponteiro = 0;
     }
-    
-    public Transacao gerarTransacao(int quantidade) {
-        long timeStamp = Instant.now().toEpochMilli();
-        Transacao transacao = new Transacao(timeStamp);
-        
+
+    public Transacao gerarTransacao() {
+
+        //Gera a transacao com seu TimeStamp
+        long timeStamp = Instant.now().toEpochMilli() + ponteiro;
+        Transacao transacao = new Transacao(ponteiro, timeStamp);
+        ponteiro++;
+
+        //Popula a transacao com operacoes aleatórias
+        Operacao operacao;
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            int aleatorio = random.nextInt();
+            operacao = new Operacao();
+            if (aleatorio % 2 == 0) { //Sorteia o tipo da operacao
+                operacao.setTipo(TipoOperacao.R);
+            } else {
+                operacao.setTipo(TipoOperacao.W);
+                operacao.setValor(aleatorio % 100); //Sorteia o valor a ser escrito
+            }
+            switch (aleatorio % 5) { //Sorteia o dado a ser alterado
+                case 0:
+                    operacao.setDado('A');
+                    break;
+                case 1:
+                    operacao.setDado('B');
+                    break;
+                case 2:
+                    operacao.setDado('C');
+                    break;
+                case 3:
+                    operacao.setDado('D');
+                    break;
+                default:
+                    operacao.setDado('E');
+            }
+            transacao.addOperacao(operacao);
+        }
+
         return transacao;
     }
-    
+
 }
