55c3ad4c33aaed9cae6d73a54721280cc66d9167
Adriano <Adriano@PC-Adriano>
2017-06-23 12:49:53 -0300

    Incluir addoOperacao() em Log

--- src/timestamp/Log.java
+++ src/timestamp/Log.java
@@ -5,10 +5,44 @@
  */
 package timestamp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Adriano
  */
 public class Log {
-    
+
+    private ArrayList<Operacao> operacoes;
+
+    public Log() {
+        this.operacoes = new ArrayList<>();
+    }
+
+    public void addOperacao(Transacao transacao, Operacao operacao) {
+        this.operacoes.add(operacao);        
+                
+        String tid = String.valueOf(transacao.getId());
+        TipoOperacao tipo = operacao.getTipo();
+        
+        String saida = tipo + tid;
+        
+        char dado = operacao.getDado();
+        int valor = operacao.getValor();
+        
+        switch(tipo) {
+            case S:
+                System.out.println(saida);
+                break;
+            case R:
+                System.out.println(saida + "(" + dado + ", " + valor + "); ");
+                break;
+            case W:
+                System.out.println(saida + "(" + dado + ", " + valor + "); ");
+                break;
+            default:
+                System.out.println(saida);
+        }
+    }
+
 }
